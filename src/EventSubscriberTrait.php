1fac8929218889952ab4145e9568434c6e4b1916
Greg Brown <greg@12dnetworks.com>
2016-12-05 20:52:12 -0800

    convenience class for event subscribers to automatically call methods if they exist based on provided event

---
+++ src/EventSubscriberTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Gdbots\Pbjx;
+
+use Gdbots\Schemas\Pbjx\Mixin\Event\Event;
+
+trait EventSubscriberTrait
+{
+    /**
+     * @param Event $event
+     * @param Pbjx $pbjx
+     */
+    public function onEvent(Event $event, Pbjx $pbjx)
+    {
+        $method = 'on' . ucfirst($event::schema()->getHandlerMethodName(false));
+        if (is_callable([$this, $method])) {
+            $this->$method($event, $pbjx);
+        }
+    }
+
+    /**
+     * @param Event[] $events
+     * @param Pbjx $pbjx
+     */
+    public function onEvents(array $events, Pbjx $pbjx)
+    {
+        foreach ($events as $event) {
+            $this->onEvent($event, $pbjx);
+        }
+    }
+}
